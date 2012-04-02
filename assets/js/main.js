0da9fddf7e29fc918adba0458da6a1b287b0bc12
Denis Evsyukov <denis@evsyukov.org>
2012-04-02 22:25:37 +0400

    Add g+ sharing link and change twitter link
    Former-commit-id: a7760fea3312d2a1f38c60ad58fa73d07fda4c31

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,17 @@ $.getScript("//platform.twitter.com/widgets.js", function(){
 });
 
 document.getElementById("year").firstChild.data = (new Date).getFullYear();
+
+(function() {
+  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+  po.src = 'https://apis.google.com/js/plusone.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
+//<![CDATA[
+var campisi = document.createElement('g:plusone'); 
+campisi.setAttribute("align","left");
+campisi.setAttribute("size","medium");
+campisi.setAttribute("annotation","none");
+document.getElementById("gplusone").appendChild(campisi);
+//]]>
