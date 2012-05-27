f29a128d0081492053c8075b895680b516bf68d0
Denis Evsyukov <denis@evsyukov.org>
2012-05-27 10:17:55 +0400

    Remove social button and google translate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,3 @@
 if(!/android|iphone|ipod|ipad/i.test(navigator.userAgent)){$(document).ready(function(){$('a[href$=".png"],a[href$=".jpg"],a[href$=".jpeg"],a[href$=".gif"]').fancybox();});}
 
-$.getScript("//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit", function(){
-  function googleTranslateElementInit() {
-    new google.translate.TranslateElement({
-      pageLanguage: 'ru',
-      multilanguagePage: true
-    });
-  }
-});
-
-$.getScript("//platform.twitter.com/widgets.js", function(){
-});
-
-document.getElementById("year").firstChild.data = (new Date).getFullYear();
-
-(function() {
-  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/plusone.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
-
-//<![CDATA[
-var campisi = document.createElement('g:plusone'); 
-campisi.setAttribute("align","left");
-campisi.setAttribute("size","medium");
-campisi.setAttribute("annotation","none");
-document.getElementById("gplusone").appendChild(campisi);
-//]]>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
