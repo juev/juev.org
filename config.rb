fcf08bf1caafddc9affe640ff221ed1617b27cc4
Evsyukov Denis <denis@evsyukov.org>
2015-06-06 22:44:56 +0400

    Do not use inline images

--- config.rb
+++ config.rb
@@ -3,13 +3,13 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/"
-http_images_path = "/images"
+http_images_path = "/assets/images"
 http_fonts_path = "/assets/fonts"
-css_dir = "dist/css"
+css_dir = "/source/assets/css"
 
 # Local development paths
 sass_dir = "assets/scss"
-images_dir = "assets/images"
+images_dir = "source/assets/images"
 fonts_dir = "source/assets/fonts"
 
 line_comments = false
