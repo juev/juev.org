ab1b5228ea37681d173fb6954457adf9faf84a57
Evsyukov Denis <114635+juev@users.noreply.github.com>
2019-01-29 08:48:02 +0300

    Change in site configuration (#10)
    * Change structure again
    
    * Some changes for test deploy
    
    * Update env
    
    * Change include directive on config file
    
    * Change id for _files
    
    * Do not store history
    
    * Added compress for html
    
    * Change id for files
    
    * Do not include _files directory
    
    * Change id for files agein
    
    * Use files from parent folder
    
    * trying to use files from _files
    
    * trying to use files from _files
    
    * Change permalink for files
    
    * Move ico to assets/ico folder
    
    * Trying to use assets from _pages
    
    * Move ico to _files
    
    * Move sass settings to rott
    
    * Compress assets
    
    * Change frontmatter
    
    * Change in site configuration
    
    
    Former-commit-id: f593883ae95872be9dea589cc14e6c73ca5cbad4
    Former-commit-id: 5433ee6f1b4183534999b60b1d48a4a492687571

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-Shadowbox.init();
-var links = document.getElementsByTagName('a');
-for(var i=0;i<links.length;i++)
-{
-  if (links[i].href.match(/(gif|png|jpg|jpeg)$/))
-  {
-    links[i].setAttribute('rel', 'shadowbox');
-  }
-}
